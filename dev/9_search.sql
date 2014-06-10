dc77f0f0a104d776885dfc086cccc4847bcfb71f
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-06-10 19:28:17 +0400

    refactor search and history fns to CTE

--- dev/9_search.sql
+++ dev/9_search.sql
@@ -80,6 +80,14 @@ DECLARE
 BEGIN
   EXECUTE
     eval_template($SQL$
+      WITH entries AS
+      (SELECT
+        x.logical_id as id
+        ,x.last_modified_date as last_modified_date
+        ,x.published as published
+        ,x.data as content
+        FROM {{tbl}} x
+        WHERE logical_id IN ({{search_sql}}))
       SELECT
         json_build_object(
           'title', 'search',
@@ -88,14 +96,7 @@ BEGIN
           'id', gen_random_uuid(),
           'entry', COALESCE(json_agg(y.*), '[]'::json)
         ) as json
-        FROM (
-          SELECT
-             x.logical_id as id
-            ,x.last_modified_date as last_modified_date
-            ,x.published as published
-            ,x.data as content
-          FROM {{tbl}} x
-          WHERE logical_id IN ({{search_sql}})) y
+        FROM entries y
    $SQL$,
   'tbl', lower(resource_type),
   'search_sql', coalesce(
@@ -115,6 +116,22 @@ DECLARE
 BEGIN
   EXECUTE
     eval_template($SQL$
+      WITH entries AS
+      (SELECT
+          x.logical_id as id
+          ,x.last_modified_date as last_modified_date
+          ,x.published as published
+          ,x.data as content
+        FROM {{tbl}} x
+        WHERE x.logical_id  = $1
+        UNION
+        SELECT
+          x.logical_id as id
+          ,x.last_modified_date as last_modified_date
+          ,x.published as published
+          ,x.data as content
+        FROM {{tbl}}_history x
+        WHERE x.logical_id  = $1)
       SELECT
         json_build_object(
           'title', 'search',
@@ -123,23 +140,7 @@ BEGIN
           'id', gen_random_uuid(),
           'entry', COALESCE(json_agg(y.*), '[]'::json)
         ) as json
-        FROM (
-          SELECT
-             x.logical_id as id
-            ,x.last_modified_date as last_modified_date
-            ,x.published as published
-            ,x.data as content
-            FROM {{tbl}} x
-            WHERE x.logical_id  = $1
-          UNION
-          SELECT
-             x.logical_id as id
-            ,x.last_modified_date as last_modified_date
-            ,x.published as published
-            ,x.data as content
-            FROM {{tbl}}_history x
-            WHERE x.logical_id  = $1
-        ) y
+        FROM entries y
    $SQL$, 'tbl', lower(_resource_type))
   INTO res USING _id;
 
