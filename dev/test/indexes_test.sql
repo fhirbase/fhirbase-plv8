8a3bd3642c3fc90918dbfea8bcc82ed38816512f
niquola <niquola@gmail.com>
2014-07-07 16:53:22 +0400

    index test; remove curl

--- dev/test/indexes_test.sql
+++ dev/test/indexes_test.sql
@@ -2,6 +2,6 @@
 
 SET escape_string_warning=off;
 --{{{
-\set pt `curl http://www.hl7.org/implement/standards/fhir/patient-example.json`
+\set pt `cat test/fixtures/pt.json`
 SELECT unnest(index_token_resource(:'pt'::jsonb));
 --}}}
