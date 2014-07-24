4a6cc9287fab286678fefd6f09922cd22437f5a9
niquola <niquola@gmail.com>
2014-07-24 12:44:00 +0400

    add search string parsing

---
+++ dev/08_search_params.sql
@@ -0,0 +1,61 @@
+--db:fhirb
+--{{{
+
+CREATE OR REPLACE FUNCTION url_decode(input text) RETURNS text
+LANGUAGE plpgsql IMMUTABLE STRICT AS $$
+DECLARE
+bin bytea = '';
+byte text;
+BEGIN
+  FOR byte IN (select (regexp_matches(input, '(%..|.)', 'g'))[1]) LOOP
+    IF length(byte) = 3 THEN
+      bin = bin || decode(substring(byte, 2, 2), 'hex');
+    ELSE
+      bin = bin || byte::bytea;
+    END IF;
+  END LOOP;
+  RETURN convert_from(bin, 'utf8');
+END
+$$;
+
+CREATE OR REPLACE
+FUNCTION _get_modifier(_key_ text) RETURNS text
+LANGUAGE sql AS $$
+  SELECT nullif(split_part(_last(regexp_split_to_array(_key_,'\.')), ':',2), '');
+$$ IMMUTABLE;
+
+CREATE OR REPLACE
+FUNCTION _get_key(_key_ text) RETURNS text
+LANGUAGE sql AS $$
+  SELECT array_to_string(_butlast(a) || split_part(_last(a), ':', 1), '.')
+from regexp_split_to_array(_key_, '\.') a;
+$$ IMMUTABLE;
+
+CREATE OR REPLACE
+FUNCTION _parse_param(_params_ text) RETURNS jsonb
+LANGUAGE sql AS $$
+
+WITH initial AS (
+  SELECT url_decode(split_part(x,'=',1)) as key,
+  url_decode(split_part(x, '=', 2)) as val
+  FROM regexp_split_to_table(_params_,'&') x
+  ), with_op_mod AS (
+  SELECT  _get_key(key) as key,
+  _get_modifier(key) as mod,
+  CASE WHEN val ~ E'^(>=|<=|<|>|~).*' THEN
+    regexp_replace(val, E'^(>=|<=|<|>|~).*','\1')
+  ELSE
+    NULL
+  END as op,
+  regexp_replace(val, E'^(>|<|<=|>=|~)(.*)','\2') as val
+  FROM  initial
+)
+SELECT json_agg(
+  json_build_object(
+    'param', key,
+    'op',    COALESCE(op,mod,'='),
+    'value', val))::jsonb
+FROM with_op_mod;
+
+$$ IMMUTABLE;
+--}}}
---
+++ dev/test/search_params_test.sql
@@ -0,0 +1,141 @@
+--db:fhirb -e
+SET escape_string_warning=off;
+--{{{
+select _butlast(a) || split_part(_last(a), ':', 1)
+from regexp_split_to_array('subject', '\.') a;
+--}}}
+--{{{
+SELECT assert_eq(
+  'text',
+  _get_modifier('subject:Patient.organization:Organization.identifier:text'::text),
+  '_get_modifier text');
+
+SELECT assert_eq(
+  NULL,
+  _get_modifier('subject:Patient.organization:Organization.identifier'::text),
+  '_get_modifier null');
+
+SELECT assert_eq(
+  'subject:Patient.organization:Organization.identifier',
+  _get_key('subject:Patient.organization:Organization.identifier:text'::text),
+  '_get_key text');
+
+SELECT assert_eq(
+  'subject:Patient.organization:Organization.identifier',
+  _get_key('subject:Patient.organization:Organization.identifier'::text),
+  '_get_key null');
+--}}}
+
+
+-- number
+SELECT assert_eq(
+  '[{"param":"param","op": "=", "value": "num"}]'::jsonb,
+  _parse_param('param=num'),
+  'numer equal');
+SELECT assert_eq(
+  '[{"op": "=", "value": "num", "param":"param"}, {"op":"=", "value":"num2", "param":"param"}]'::jsonb,
+  _parse_param('param=num&param=num2'),
+  'numer equal');
+SELECT assert_eq(
+  _parse_param('param=<num'),
+  '[{"param":"param", "op": "<" ,"value":"num"}]'::jsonb,
+  'number less');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "<=" ,"value":"num"}]'::jsonb,
+  _parse_param('param=<%3Dnum'),
+  'number less or equal');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": ">" ,"value":"num"}]'::jsonb,
+  _parse_param('param=>num'),
+  'number more');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": ">=" ,"value":"num"}]'::jsonb,
+  _parse_param('param=>%3Dnum'),
+  'number more or equal');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "missing", "value": "true"}]'::jsonb,
+  _parse_param('param:missing=true'),
+  'number missing');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "missing", "value": "false"}]'::jsonb,
+  _parse_param('param:missing=false'),
+  'number present');
+
+-- date
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "=" ,"value": "date"}]'::jsonb,
+  _parse_param('param=date'),
+  'date equal');
+SELECT assert_eq(
+  '[{"param":"param", "op": "<" ,"value": "date"}]'::jsonb,
+  _parse_param('param=<date'),
+  'date less');
+SELECT assert_eq(
+  '[{"param":"param", "op": "<=" ,"value": "date"}]'::jsonb,
+  _parse_param('param=<%3Ddate'),
+  'date less or equal');
+SELECT assert_eq(
+  '[{"param":"param", "op": ">" ,"value": "date"}]'::jsonb,
+  _parse_param('param=>date'),
+  'date more');
+SELECT assert_eq(
+  '[{"param": "param", "op": ">=" ,"value": "date"}]'::jsonb,
+  _parse_param('param=>%3Ddate'),
+  'date more or equal');
+SELECT assert_eq(
+  '[{"param":"param", "op": "missing", "value":"true"}]'::jsonb,
+  _parse_param('param:missing=true'),
+  'date missing');
+SELECT assert_eq(
+  '[{"param":"param", "op": "missing", "value":"false"}]'::jsonb,
+  _parse_param('param:missing=false'),
+  'date present');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "=" ,"value": "str"}]'::jsonb,
+  _parse_param('param=str'),
+  'str equal');
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "exact" ,"value": "str"}]'::jsonb,
+  _parse_param('param:exact=str'),
+  'str exact');
+
+--{{{
+SELECT assert_eq(
+  '[{"param":"subject:Patient.name", "op": "=", "value":"ups"}]'::jsonb,
+  _parse_param('subject:Patient.name=ups'),
+  'path');
+--}}}
+
+-- token
+
+-- quantity
+
+SELECT assert_eq(
+  '[{"param":"param", "op": "~" ,"value": "quantity"}]'::jsonb,
+  _parse_param('param=~quantity'),
+  'quantity approximation');
+
+-- reference
+
+/* ### Chained params ### */
+
+/* [param].[rest]=[val] {"param":"param", "op": "chain" ,"value": "#{rest}=#{val}"} */
+/* [param]:[type].[rest]=[val] {"param":"param", "op": "chain" ,"value": [type "#{rest}=#{val}"]} */
+/* [param]:[type].[rest]:modifier=[val] {"param":"param", "op": "modifier" ,"value": [type "#{rest}=#{val}"]} */
+
+/* ### Composite Search params ### */
+
+/* [first]&[rest] {:key "op": "and" ,"value":ue [first, rest]} */
+/* [param]=[val,rest] {:key "op": "ORDER BY" ,"value":ue []} */
+/* [param]=key1$val1,key2$val2 {:kep param "op": "composite" ,"value": [key1 val1 key2 val2]} */
+
+
+--}}}
