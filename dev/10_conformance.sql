ba26a78012e68e540a7a828620857541902a9708
niquola <niquola@gmail.com>
2014-08-19 15:58:46 +0400

    fixes #36; generate profile

--- dev/10_conformance.sql
+++ dev/10_conformance.sql
@@ -55,6 +55,28 @@ $$;
 CREATE OR REPLACE
 FUNCTION fhir_profile(_cfg jsonb, _resource_name_ text) RETURNS jsonb
 LANGUAGE sql AS $$
-SELECT null::jsonb
+WITH elems AS (
+  SELECT array_to_string(e.path,'.') as path,
+         json_build_object(
+           'min', e.min,
+           'max', e.max,
+           'type', (SELECT json_agg(tt.*) FROM (SELECT t as code FROM unnest(e.type) t) tt)
+         ) as definition
+    FROM fhir.resource_elements e
+   WHERE path[1]=_resource_name_
+), params AS (
+  SELECT sp.name, sp.type, sp.documentation
+  FROM fhir.resource_search_params sp
+    WHERE sp.path[1] = _resource_name_
+)
+SELECT json_build_object(
+   'name', _resource_name_,
+   'structure', ARRAY[json_build_object(
+     'type', _resource_name_,
+     'publish', true,
+     'element', (SELECT json_agg(t.*) FROM elems  t),
+     'searchParam',  (SELECT  json_agg(t.*)  FROM params t)
+   )]
+)::jsonb
 $$;
 --}}}
--- dev/test/conformance_test.sql
+++ dev/test/conformance_test.sql
@@ -4,6 +4,12 @@ SET escape_string_warning=off;
 
 --{{{
 BEGIN;
-  SELECT assert_eq('version', (SELECT fhir_conformance('{"version":"version"}'))->>'version', 'version');
+
+SELECT assert_eq('version', (SELECT fhir_conformance('{"version":"version"}'))->>'version', 'version');
+
+SELECT assert_eq('Patient',
+   (fhir_profile(null::jsonb, 'Patient')#>>'{structure,0,element,0,path}'),
+  'conformance');
 ROLLBACK;
+
 --}}}
