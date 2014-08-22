dde0273ee1c1e59dfe801d279ceca2ce023d5f66
niquola <niquola@gmail.com>
2014-08-22 19:01:24 +0400

    fix conformance using fhirplace

--- dev/10_conformance.sql
+++ dev/10_conformance.sql
@@ -60,23 +60,28 @@ WITH elems AS (
          json_build_object(
            'min', e.min,
            'max', e.max,
-           'type', (SELECT json_agg(tt.*) FROM (SELECT t as code FROM unnest(e.type) t) tt)
+           'type', COALESCE((SELECT json_agg(tt.*) FROM (SELECT t as code FROM unnest(e.type) t) tt), '[]'::json)
          ) as definition
     FROM fhir.resource_elements e
    WHERE path[1]=_resource_name_
 ), params AS (
-  SELECT sp.name, sp.type, sp.documentation
+  SELECT sp.name, sp.type, sp.documentation, array_to_string(sp.path, '/') as xpath
   FROM fhir.resource_search_params sp
     WHERE sp.path[1] = _resource_name_
 )
 SELECT json_build_object(
    'name', _resource_name_,
+   'resourceType', 'Profile',
    'structure', ARRAY[json_build_object(
      'type', _resource_name_,
      'publish', true,
-     'element', (SELECT json_agg(t.*) FROM elems  t),
+     'differential', json_build_object(
+       'element', (SELECT json_agg(t.*) FROM elems  t)
+     ),
      'searchParam',  (SELECT  json_agg(t.*)  FROM params t)
    )]
 )::jsonb
 $$;
+
+SELECT fhir_profile('{}', 'Patient');
 --}}}
