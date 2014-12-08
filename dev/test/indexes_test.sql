78bdb7336febad35504133f4c343257fbd563cbe
niquola <niquola@gmail.com>
2014-12-08 14:30:56 +0400

    merge dup tests

--- dev/test/indexes_test.sql
+++ dev/test/indexes_test.sql
@@ -55,3 +55,23 @@ SELECT assert_eq(
 true,
 'index_as_string');
 --}}}
+
+--{{{
+\set pt `cat test/fixtures/pt.json`
+
+SELECT assert_eq(
+(SELECT index_as_reference(:'pt'::jsonb,  '{managingOrganization}'::varchar[])),
+'{550e8400-e29b-41d4-a716-446655440009,Organization/550e8400-e29b-41d4-a716-446655440009}',
+'index reference');
+
+
+/* SELECT index_as_string(:'pt'::jsonb,  '{name}'::varchar[]); */
+
+SELECT assert_eq(
+(SELECT
+  regexp_matches(
+  index_as_string(:'pt'::jsonb,  '{name}'::varchar[]),
+  'Roel')),
+'{Roel}'::text[],
+'index_as_string');
+--}}}
