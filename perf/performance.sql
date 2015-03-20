2747cf42dbdeedd48f738d033134dfaa133bb406
Danil Kutkevich <danil@kutkevich.org>
2015-03-20 12:59:39 +0300

    FIX: performance: create_patient_with_id function

--- perf/performance.sql
+++ perf/performance.sql
@@ -8,6 +8,15 @@ proc! create_patients(_limit_ integer) RETURNS void
                               jsonbext.dissoc(patients.content, 'id')))
             FROM (SELECT content FROM patient LIMIT _limit_) patients;
 
+proc! create_patient_with_id(_id_ text) RETURNS void
+  BEGIN
+    RAISE NOTICE 'Create patient with id';
+    DELETE FROM patient WHERE content#>>'{id}' = _id_;
+    PERFORM count(crud.create('{}'::jsonb,
+                              jsonbext.merge(patients.content,
+                                             ('{"id": "' || _id_ || '"}')::jsonb)))
+            FROM (SELECT content FROM patient LIMIT 1) patients;
+
 proc! read_patients(_limit_ integer) RETURNS void
   BEGIN
     RAISE NOTICE 'Read patients';
@@ -53,20 +62,17 @@ proc! search_patient_with_only_one_search_candidate() RETURNS void
     PERFORM count(*)
             FROM fhir.search('Patient', 'name=foobarbaz&_count=50000000');
 
+proc! index_search_param(_resource_type_ text, _name_ text) RETURNS void
+  BEGIN
+    RAISE NOTICE 'Search Patient by partial match and with only one search candidate';
+    PERFORM indexing.drop_index_search_param(_resource_type_, _name_);
+    PERFORM indexing.index_search_param(_resource_type_, _name_);
+
 -- -- FIXME: Takes to many time!
 -- SELECT indexing.index_search_param('Patient','birthdate');
 
 -- -- FIXME: Takes to many time!
 -- SELECT indexing.index_search_param('Patient','identifier');
-
-proc! history_for_one_patient() RETURNS void
-  BEGIN
-    RAISE NOTICE 'History for one patient';
-    PERFORM count(crud.create('{}'::jsonb,
-                              jsonbext.merge(patients.content,
-                                             '{"id": "foo-bar-id"}'::jsonb)))
-            FROM (SELECT content FROM patient LIMIT 1) patients;
-    PERFORM crud.history('{}'::jsonb, 'Patient', 'foo-bar-id');
 
 -- -- FIXME: Takes to many time and waste all disk space!
 -- SELECT count(crud.history('{}'::jsonb, 'Patient'));
