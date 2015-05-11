7baf3eb24fb1d16c489dc4dc98bb59fc2978ceb3
niquola <niquola@gmail.com>
2015-05-11 20:48:51 +0300

    use js in terminology

--- src/fhirbase_terminology.sql
+++ src/fhirbase_terminology.sql
@@ -1,62 +1,44 @@
--- #import ./fhirbase_json.sql
 -- #import ./fhirbase_crud.sql
 
-proc include_filter(_system_ text, _version_ text, filters jsonb) RETURNS text
-  BEGIN
-    RETURN '{}'::jsonb;
+jsfn expand(_vs_id text, _filter_ text) RETURNS json
+  var p = function(x){ plv8.elog(WARNING, JSON.stringify(x)); }
+  var r = plv8.execute("SELECT content::json FROM valueset WHERE logical_id = $1", _vs_id)[0];
+  if(!r){ return null; }
 
-func _expand_define(_vs_ jsonb, _filter_ text) RETURNS jsonb
-  select
-    fhirbase_json.assoc(
-      _vs_,
-      'expansion', json_build_object(
-        'identifier', '???',
-        'timestamp', '???',
-        'contains', coalesce(json_agg(y.*), '[]')
-      )::jsonb
-    ) from (
-      select
-        _vs_#>>'{define,system}' as system
-        , x->>'code' as code
-        , x->>'display' as display
-        , x->>'definition' as definition
-      from jsonb_array_elements(_vs_#>'{define,concept}') as x
-      WHERE  _filter_ = ''
-        OR (x->>'display' ilike '%' || _filter_ || '%' OR  x->>'code'    ilike '%' || _filter_ || '%')
-  ) y
+  var fltr = _filter_.toLowerCase();
 
-func _is_simple_compose(_vs_ jsonb) RETURNS boolean
-  SELECT bool_and((x->'concept') IS NOT NULL)
-    FROM jsonb_array_elements(_vs_#>'{compose,include}') x
+  function smatch(s){
+    return s && s.toLowerCase().indexOf(fltr) > -1;
+  }
 
-func _expand_simple_compose(_vs_ jsonb, _filter_ text) RETURNS jsonb
-  SELECT  fhirbase_json.assoc(
-            _vs_,
-            'expansion', json_build_object(
-              'identifier', '???',
-              'timestamp', '???',
-              'contains', json_agg(fhirbase_json.assoc(y.concept, 'system', y.system))
-            )::jsonb)
-    FROM (
-      SELECT *
-        FROM (SELECT x->'system' as system,
-                    jsonb_array_elements(x->'concept') concept
-                FROM jsonb_array_elements(_vs_#>'{compose,include}') x
-              ) _
-        WHERE  _filter_ = ''
-          OR (concept->>'display' ilike '%' || _filter_ || '%' OR  concept->>'code'    ilike '%' || _filter_ || '%')
-    ) y
+  function match(c){
+    return smatch(c.code) || smatch(c.display);
+  }
 
-proc expand(_vs_id_ text, _filter_ text) RETURNS jsonb
-  _vs_ jsonb;
-  BEGIN
-    SELECT content INTO _vs_
-      FROM valueset where logical_id = _vs_id_;
+  var vs = r.content;
+  var codes = [];
 
-    IF (_vs_->>'define') is not null AND (_vs_->>'compose') is null THEN
-      RETURN this._expand_define(_vs_, _filter_);
-    ELSIF this._is_simple_compose(_vs_) THEN
-      RETURN this._expand_simple_compose(_vs_, _filter_);
-    ELSE
-      RETURN '{"resourceType": "OperationOutcome", "message": "Not implemented"}'::jsonb;
-    END IF;
+  if(vs.define && vs.define.concept){
+    vs.define && vs.define.concept && vs.define.concept.forEach(function(c){
+      if(match(c)){
+        c.system = vs.define.system;
+        codes.push(c);
+      }
+    });
+  }
+  if(vs.compose && vs.compose.include){
+    vs.compose.include.forEach(function(x){
+      x.concept && x.concept.forEach(function(c){
+        if(match(c)){
+          c.system = x.system;
+          codes.push(c);
+        }
+      });
+    });
+  }
+  vs.expansion = {
+    identifier: '???',
+    timestamp: '???',
+    contains: codes
+  }
+  return vs;
--- test/fhirbase_terminology_spec.sql
+++ test/fhirbase_terminology_spec.sql
@@ -1,35 +1,38 @@
 expect
- jsonb_array_length(
+ json_array_length(
    fhirbase_terminology.expand(
-    'diagnostic-report-status',
-    ''
+    'diagnostic-report-status', ''
    )#>'{expansion, contains}'
  ) > 0
 => true
 
 expect
- jsonb_array_length(
+ json_array_length(
    fhirbase_terminology.expand(
-    'diagnostic-report-status',
-    'unexist'
+    'diagnostic-report-status', 'unexist'
    )#>'{expansion, contains}'
  ) > 0
 => false
 
 expect
- jsonb_array_length(
+ json_array_length(
    fhirbase_terminology.expand('v3-Race','india')#>'{expansion, contains}'
  ) > 0
 => true
 
 expect
-  jsonb_array_length(
+  json_array_length(
     fhirbase_terminology.expand('valueset-systems','')#>'{expansion, contains}'
   ) > 60
 => true
 
 expect
-  jsonb_array_length(
+  json_array_length(
     fhirbase_terminology.expand('modality','CR')#>'{expansion, contains}'
   ) = 1
 => true
+
+select count(*) from (
+  select fhirbase_terminology.expand(logical_id, '')
+    from valueset
+) _;
