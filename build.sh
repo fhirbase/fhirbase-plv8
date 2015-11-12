c03d2a9f68332c05050a627f858aab665777c6aa
niquola <niquola@gmail.com>
2015-11-12 09:48:22 +0000

    tech: add tests to build

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ coffee  utils/generate_migrations.coffee -n > tmp/schema.sql
 plpl/bin/plpl compile tmp/code.sql
 cat tmp/schema.sql > tmp/build.sql
 cat tmp/code.sql >> tmp/build.sql
-rm tmp/code.sql
-rm tmp/schema.sql
 
-
+psql postgres -c 'drop database build' && psql postgres -c "create database build with ENCODING = 'UTF-8' LC_CTYPE = 'ru_RU.UTF-8' LC_COLLATE = 'ru_RU.UTF-8'   template = template0" && cat tmp/build.sql | psql build && npm run test
 
