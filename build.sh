762763afffae0960d32f64ed37274f8b39a99c0b
Danil Kutkevich <danil@kutkevich.org>
2016-01-19 19:10:17 +0300

    fix: More respect to multitenancy (#80).
    Build and release scripts do not rely on setting schema definition in
    final sql file. Move `SET search_path TO schema_name` from sql file
    to release script.

--- build.sh
+++ build.sh
@@ -3,17 +3,11 @@ coffee  utils/generate_migrations.coffee -n > tmp/schema.sql
 cp utils/patch_3.sql tmp/patch_3.sql
 plpl/bin/plpl compile tmp/code.sql
 
-FB_SCHEMA=${FB_SCHEMA:-public}
-schema_statement="CREATE SCHEMA IF NOT EXISTS $FB_SCHEMA;"
-schema_statement="$schema_statement SET search_path TO $FB_SCHEMA;"
-
-echo "$schema_statement" > tmp/build.sql
-cat tmp/schema.sql >> tmp/build.sql
+cat tmp/schema.sql > tmp/build.sql
 cat tmp/patch_3.sql >> tmp/build.sql
 cat tmp/code.sql >> tmp/build.sql
 
-echo "$schema_statement" > tmp/patch.sql
-cat tmp/patch_3.sql >> tmp/patch.sql
+cat tmp/patch_3.sql > tmp/patch.sql
 cat tmp/code.sql >> tmp/patch.sql
 
 # psql postgres -c 'drop database build' && psql postgres -c "create database build with ENCODING = 'UTF-8' LC_CTYPE = 'ru_RU.UTF-8' LC_COLLATE = 'ru_RU.UTF-8'   template = template0" && cat tmp/build.sql | psql build && npm run test
--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ set -e
 # DATABASE_URL=postgres://your_user_name:your_password@localhost:5432/fhirbase_build
 # WARNING: `fhirbase_build` database will be destroed and recreated!
 
-PREV_FBVERSION="fhirbase-0.0.1-beta.8"
-FBVERSION="fhirbase-0.0.1-beta.9"
+PREV_FBVERSION="0.0.1-beta.9"
+FBVERSION="0.0.1-beta.10"
 
 PGOPTIONS='--client-min-messages=warning'
 loadcmd="psql --no-psqlrc --quiet --echo-all --single-transaction \
@@ -18,32 +18,49 @@ loadcmd="psql --no-psqlrc --quiet --echo-all --single-transaction \
 # because `fhirbase_build` database will be droped.
 OTHER_DATABASE_URL="${DATABASE_URL/%\/fhirbase_build/\/postgres}"
 
+function schema_statement {
+    local schema=${1:-public}
+
+    statement="CREATE SCHEMA IF NOT EXISTS $schema;"
+    statement="$statement SET search_path TO $schema;"
+    echo -n "$statement"
+}
+
 psql "$OTHER_DATABASE_URL" --command='DROP DATABASE IF EXISTS fhirbase_build' || exit 1
 psql "$OTHER_DATABASE_URL" --command='CREATE DATABASE fhirbase_build' || exit 1
 
 FB_SCHEMA=public bash build.sh || exit 1
-cat releases/$PREV_FBVERSION.sql | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
-time cat tmp/patch.sql | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
+curl --location \
+     --output ./releases/fhirbase-$PREV_FBVERSION.sql.zip \
+     https://github.com/fhirbase/fhirbase-plv8/releases/download/v$PREV_FBVERSION/fhirbase-$PREV_FBVERSION.sql.zip || exit 1
+unzip ./releases/fhirbase-$PREV_FBVERSION.sql.zip -d ./releases/ || exit 1
+{ echo $(schema_statement "public"); cat releases/fhirbase-$PREV_FBVERSION.sql; } \
+    | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
+{ echo $(schema_statement "public"); cat tmp/patch.sql; } \
+    | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
 FB_SCHEMA=public npm run test || exit 1
 
 psql "$OTHER_DATABASE_URL" --command='DROP DATABASE IF EXISTS fhirbase_build' || exit 1
 psql "$OTHER_DATABASE_URL" --command='CREATE DATABASE fhirbase_build' || exit 1
 
 FB_SCHEMA=foo bash build.sh || exit 1
-cat tmp/build.sql | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
+{ echo $(schema_statement "foo") ; cat tmp/build.sql; } \
+    | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
 FB_SCHEMA=foo npm run test || exit 1
 
 FB_SCHEMA=bar bash build.sh || exit 1
-cat tmp/build.sql | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
+{ echo $(schema_statement "bar") ; cat tmp/build.sql; } \
+    | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
 FB_SCHEMA=bar npm run test || exit 1
 
 FB_SCHEMA=public bash build.sh || exit 1
-cat tmp/build.sql | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
+{ echo $(schema_statement "public") ; cat tmp/build.sql; } \
+    | $loadcmd "$DATABASE_URL" > /dev/null || exit 1
 FB_SCHEMA=public npm run test || exit 1
 
-cp tmp/build.sql  releases/$FBVERSION.sql || exit 1
-cp tmp/patch.sql  releases/$FBVERSION-patch.sql || exit 1
+cp tmp/build.sql releases/fhirbase-$FBVERSION.sql || exit 1
+cp tmp/patch.sql releases/fhirbase-$FBVERSION-patch.sql || exit 1
 cd releases || exit 1
 
-zip -r $FBVERSION.sql.zip zip $FBVERSION.sql || exit 1
-zip -r $FBVERSION-patch.sql.zip $FBVERSION-patch.sql
+zip -r fhirbase-$FBVERSION.sql.zip zip fhirbase-$FBVERSION.sql || exit 1
+zip -r fhirbase-$FBVERSION-patch.sql.zip fhirbase-$FBVERSION-patch.sql
