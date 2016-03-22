70efbe77e94529b70791a2623ee39ba30c1e3053
Danil Kutkevich <danil@kutkevich.org>
2016-03-22 19:49:48 +0300

    minor

--- provisioning/docker_setup.sh
+++ provisioning/docker_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker stop fhirbase-ansible
 docker rm fhirbase-ansible
-docker build --tag fhirbase-ansible .
-docker create --tty --publish=7022:22 --name fhirbase-ansible fhirbase-ansible
-docker start fhirbase-ansible
+docker build --tag fhirbase-ansible . || exit 1
+docker create --tty --publish=7022:22 --name fhirbase-ansible fhirbase-ansible || exit 1
+docker start fhirbase-ansible || exit 1
