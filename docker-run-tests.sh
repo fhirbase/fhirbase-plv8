cc192673500d7e42c1ea4e427c91dd3dc77fb8bf
Danil Kutkevich <danil@kutkevich.org>
2016-04-18 19:20:20 +0300

    chore: parameterize travis tests schemas (47)

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
+schemas="${schemas:-public}"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --schemas=*)
+            export schemas="${1#*=}"
+            ;;
+        *)
+            printf "***************************\n"
+            printf "* Error: Invalid argument.*\n"
+            printf "***************************\n"
+            exit 1
+    esac
+    shift
+done
+
 export PATH="$HOME"/fhirbase/node_modules/coffee-script/bin:"$PATH" || exit 1
 export DATABASE_URL=postgres://fhirbase:fhirbase@localhost:5432/fhirbase || exit 1
 
 sudo service postgresql start || exit 1
 cd ~/fhirbase || exit 1
 source ~/.nvm/nvm.sh && nvm use 5.3 || exit 1
-
-schemas="foo bar"
 
 for schema in $schemas; do
     FB_SCHEMA=$schema ./build.sh || exit 1
