b5b1bc540015c9b1a43455c263c6e8a5784af0f3
Danil Kutkevich <danil@kutkevich.org>
2016-06-07 20:18:04 +0300

    fix: patch should contains extensions statements

--- build-commit.sh
+++ build-commit.sh
@@ -40,6 +40,11 @@ if [[ ! -d $BUILD_DIR ]]; then
   coffee utils/generate_patch.coffee -n > $BUILD_DIR/patch.sql
 
   plpl/bin/plpl compile $BUILD_DIR/code.sql
+  # FIXME: May be extension statements should output from plpl?
+  echo -e "CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS plv8 WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;
+$(cat $BUILD_DIR/code.sql)" > $BUILD_DIR/code.sql
 
   cat $BUILD_DIR/schema.sql > $BUILD_DIR/build.sql
   cat $BUILD_DIR/patch.sql >> $BUILD_DIR/build.sql
